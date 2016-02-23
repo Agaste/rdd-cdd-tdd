79bd10727b264b67e1a0fd353beb41f1f5ef0331
Antoine Gasté <Antoine Gast>
2016-02-23 16:01:53 +0100

    updates the test files

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -2,10 +2,35 @@ var assert = require('assert'),
     config = require('./../lib/config');
 
 
+var units = {
+        temp: 1,
+        wind: 2,
+        format: 'us'
+
+    },
+    ip = '1101';
+
+var Config = config(units,ip);
+
 describe('Config', function () {
-    it('has 3 props')
+    it('has 3 props', function() {
+        assert(Object.keys(Config).length === 3); // check the we have 3 props for this object (easily understandable so I used it)
+    });
 
-    it('after construction, 2 props are populated')
+    it('after construction, 2 props are populated', function() {
+    	//var count = 0;
+/*
+        Object.keys(Config).forEach(function (key) {
+            key !== null || key !== '' ? count++ : count;
+        });
+*/
 
-    it('has correct key types')
+		assert(Config.ip!='');
+		assert(Config.units!=null);
+
+    });
+
+    it('has correct key types', function() {
+    	assert(typeof(Config.ip) == 'string' || typeof(Config.units) == 'object');
+});
 });
--- weather/test/test-format.js
+++ weather/test/test-format.js
@@ -2,5 +2,7 @@ var assert = require('assert'),
     format = require('./../lib/format');
 
 describe('format', function () {
-    it('is a constructor of the Format object')
+    it('is a constructor of the Format object',function() {
+        assert(typeof(format) === 'function'); //check whether format is a function type or not (found on the real test file and easily understandable so I used it)
+    });
 });
